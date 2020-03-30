af3401b559f6df67573e8278336df2a16afb58f2
Gamm3r96 <gamm3r96@gmail.com>
2020-03-29 23:28:38 -0500

    Create spinner-1.sh

---
+++ spinner-1.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#a spinner function
+#find me on twitter @gamm3r96
+show_spinner()
+{
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while ps a | awk '{print $1}' | grep -q "${pid}"; do
+    temp="${spinstr#?}"
+    printf " [%c]  " "${spinstr}"
+    spinstr=${temp}${spinstr%"${temp}"}
+    sleep "${delay}"
+    printf "\b\b\b\b\b\b"
+  done
+  printf "    \b\b\b\b"
+}
+
+("$@") &
+show_spinner "$!"
